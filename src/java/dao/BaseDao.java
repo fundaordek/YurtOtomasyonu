5ce1a2eac3f662ec8e903cb3ff72e1189543455f
Funda Ordek <fundaordek@gmail.com>
2021-04-01 17:41:29 +0300

    .

---
+++ src/java/dao/BaseDao.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import util.DBConnection;
+
+abstract class BaseDao {
+
+    private DBConnection connector;
+    private Connection connection;
+
+    public DBConnection getConnector() {
+        if (this.connector == null) {
+            this.connector = new DBConnection();
+        }
+
+        return connector;
+    }
+
+    public Connection getConnection() {
+        if (this.connection == null) {
+            this.connection = this.getConnector().connect();
+        }
+        return connection;
+    }
+}
